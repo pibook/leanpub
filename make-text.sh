e5c20107360a4e900ab3d27b05bea3266bf7ba7e
epidrome <github@epidro.me>
2022-01-10 16:09:39 +0200

    updated

--- make-text.sh
+++ make-text.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in text/*.md; do
+for filename in text/mi-*.md; do
    [ -e "$filename" ] || continue
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown > manuscript/"$(basename "$filename")"
 done
