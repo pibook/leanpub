0fb837b884f8e7f1d506281762186169dc0e9909
epidrome <github@epidro.me>
2022-02-05 18:55:48 +0200

    added references for figures and bibliography

--- figure.lua
+++ figure.lua
@@ -1,16 +1,17 @@
-stringify = pandoc.utils.stringify
 
 function Image(img)
+      stringify = pandoc.utils.stringify
       if img.classes:find('figure',1) then
         local fn = img.src
         local f = io.open("figures/" .. fn, 'r')
         local doc = pandoc.read(f:read('*a'))
         f:close()
         --print(fn)
-        local src=pandoc.utils.stringify(doc.meta.image_url) or "src has not been set"
+        local figid = string.sub(fn,1,string.len(fn)-3)
+        local src = stringify(doc.meta.image_url) or "src has not been set"
         src = string.sub(src,2)
-        local caption=pandoc.utils.stringify(doc.meta.caption) or "caption has not been set"
-        return pandoc.Image(caption,src)
+        local caption = stringify(doc.meta.caption) or "caption has not been set"
+        return pandoc.Image(caption,src,nil,"fig:" .. figid)
       end
 end
 
--- make-manuscript.sh
+++ make-manuscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in text/*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown > manuscript/"$(basename "$filename" .txt).md"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to markdown-citations > manuscript/"$(basename "$filename" .txt).md"
 done
