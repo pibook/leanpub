d26a40f191620d2907ac2cdac79490be2710d297
epidrome <github@epidro.me>
2022-02-07 11:09:57 +0200

    convert to strict md to add bibliography

--- make-manuscript.sh
+++ make-manuscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in text/*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=remove-notes.lua --to markdown | pandoc --metadata-file=meta.yml --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to markdown > manuscript/"$(basename "$filename" .txt).md"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=remove-notes.lua --to markdown | pandoc --metadata-file=meta.yml --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to markdown_strict > manuscript/"$(basename "$filename" .txt).md"
 done
