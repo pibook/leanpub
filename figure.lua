17c5c396d7250a117adf8234a7d5f90ee20f2642
epidrome <github@epidro.me>
2022-02-07 10:54:07 +0200

    removed pandoc flavoured notes

--- figure.lua
+++ figure.lua
@@ -7,11 +7,11 @@ function Image(img)
         local doc = pandoc.read(f:read('*a'))
         f:close()
         --print(fn)
-        local figid = string.sub(fn,1,string.len(fn)-3)
+        --local figid = string.sub(fn,1,string.len(fn)-3)
         local src = stringify(doc.meta.image_url) or "src has not been set"
         src = string.sub(src,2)
         local caption = stringify(doc.meta.caption) or "caption has not been set"
-        return pandoc.Image(caption,src,nil,"fig:" .. figid)
+        return pandoc.Image(caption,src)
       end
 end
 
---
+++ remove-notes.lua
@@ -0,0 +1,6 @@
+
+function Note(n)
+    return {} 
+end
+
+
